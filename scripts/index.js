96dcbb53c8287497d4374080c90730727bcc508a
mwturner611 <mwturner611@gmail.com>
2020-10-24 12:55:57 -0500

    clean up submit function, create reset function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// collect user input from form
 let userWords = [document.getElementById("word1"),
                 document.getElementById("word2"),
                 document.getElementById("word3"),
@@ -10,16 +11,23 @@ let userWords = [document.getElementById("word1"),
                 document.getElementById("word10")
             ];
 
+
+// When form is submitted validate contents of form and create clean array of words
 const submitForm = () => {
 
+    // enpty array for the words
     let finalArray = [];
 
+    // loop through the possible 10 words
     for (let i =0; i < 10; i++){
+
+        // ck if the words are too long and trigger message to screen
         if (userWords[i].value.length !== undefined && userWords[i].value.length > 14) {
             let word = userWords[i].value;
             alertWords("smallHeader",word);
             return;
         }
+        // if the field has an acceptable value, send it to the finalArray
         else if (userWords[i].value.length > 0 && userWords[i].value.length < 15){
 
             let word = userWords[i].value.toUpperCase().split("");
@@ -27,12 +35,11 @@ const submitForm = () => {
             finalArray.push(word);
         }
     }
-    console.log(finalArray);
+    return(finalArray);
+};
 
-}
-
+// present a message to a specific location on screen
 const alertWords = (id,word) => {
-
     let h5 = document.createElement("h5");
 
     let longWord = document.createTextNode("Please only enter words shorter than 14 characters "+ word + " is too long");
@@ -42,9 +49,13 @@ const alertWords = (id,word) => {
     document.getElementById(id).appendChild(h5).style.color = "red";
 };
 
+// clear the words in the form
 const resetForm = () => {
-    console.log("Now they'd be gone");
+    for (let i = 0; i < userWords.length; i++){
+        userWords[i].value = "";
+    }
 }
 
+//create on click events for buttons
 document.getElementById("submit").addEventListener("click", submitForm);
 document.getElementById("reset").addEventListener("click", resetForm);
